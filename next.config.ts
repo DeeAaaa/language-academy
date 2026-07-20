f2bcccffb1e70b5555a11a8682b67e48fbedf0a6
DeeAaaa <Deeaco@outlook.com>
2026-07-20 16:25:33 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/language-academy",
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
   typescript: {
     ignoreBuildErrors: true
   }
